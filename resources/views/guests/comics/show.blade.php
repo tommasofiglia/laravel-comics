f1f36eb12e685c4d49e8ebc4f668608a8dab8781
Tommaso Figlia <tommasofiglia@gmail.com>
2021-03-08 03:18:07 +0100

    Caricati gli screenshot di guest e admin

--- resources/views/guests/comics/show.blade.php
+++ resources/views/guests/comics/show.blade.php
@@ -1,4 +1,12 @@
 @extends('layouts.app')
 @section('content')
-  <h1>{{$comic->title}}</h1>
+  <div id="show_blade">
+    <div class="jumbo position-relative">
+      <img src="{{asset('storage/' . $comic->cover )}}" class="position-absolute">
+    </div>
+    <div class="under_jumbo"></div>
+    <div class="container text-uppercase">
+      <h1 class="mt-5 mb-5">{{$comic->title}}</h1>
+    </div>
+  </div>
 @endsection
